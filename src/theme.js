a0a949969aa9bd9da737b80ec2808f4b2f6ec7ca
Gracjana Kromska <gracjana0915@wp.pl>
2023-08-15 15:34:19 +0200

    added styled components

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+export const theme = {
+    color: {
+        black: "#000",
+        silver: "#ccc",
+        white: "#fff",
+        alto: "#ddd",
+        gallery: "#eee",
+        teal: "#008080",
+        bondiBlue: "#00b3b3",
+        japaneseLaurel: "#008000",
+        red: "#eb0c0c",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
