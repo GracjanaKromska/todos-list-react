8bebf6d83d00ac2627fdeac1e88f0bec99559ad4
Gracjana Kromska <gracjana0915@wp.pl>
2023-09-04 16:08:01 +0200

    search button & queryParameters hook added

---
+++ src/features/tasks/TasksPage/FetchExampleTasksButton/index.js
@@ -0,0 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
+import { fetchExampleTasks, selectLoading } from "../../tasksSlice";
+import ExampleButton from "../ExampleButton";
+
+export default () => {
+    const dispatch = useDispatch();
+    const loading = useSelector(selectLoading);
+
+    return (
+        <ExampleButton disabled={loading} onClick={() => dispatch(fetchExampleTasks())}>
+            {
+                loading
+                    ? "Ładowanie..."
+                    : "Pobierz przykładowe zadania"
+            }
+        </ExampleButton>
+    );
+};
