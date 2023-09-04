e5250acead3148454ca0ca3860b6a71d8022238b
Gracjana Kromska <gracjana0915@wp.pl>
2023-09-04 16:50:58 +0200

    cosmetic change

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -9,8 +9,7 @@ export default () => (
             body={
                 <>
                     <p>
-                        Nazywam się
-                        <strong>Gracjana Kromska</strong>. Urodziłam się 15 września 1999 roku, a więc w tym roku obchodzę swoje 24 urodziny. 
+                        Nazywam się <strong>Gracjana Kromska</strong>. Urodziłam się 15 września 1999 roku, a więc w tym roku obchodzę swoje 24 urodziny. 
                     </p>
                     <p>
                         Z zawodu jestem optykiem okularowym. Pochodzę z wielkopolski, a moim miastem rodzinnym jest Zbąszyń.
