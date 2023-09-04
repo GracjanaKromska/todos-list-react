903e4ee64e54a7ef0aee8ce54f8cab08db655843
Gracjana Kromska <gracjana0915@wp.pl>
2023-09-04 16:10:47 +0200

    updated tasksSlice.js

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -57,5 +57,20 @@ export const selectTasks = state => selectTasksState(state).tasks;
 export const selectHideDone = state => selectTasksState(state).hideDone;
 export const selectEmptyTasks = state => selectTasks(state).lenght === 0;
 export const selectEveryTaskDone = state => selectTasks(state).every(({ done }) => done);
+export const selectLoading = state => selectTasksState(state).loading;
+
+export const getTaskById = (state, taskId) =>
+    selectTasks(state).find(({ id }) => id === taskId);
+
+export const selectTasksByQuery = (state, query) => {
+    const tasks = selectTasks(state);
+
+    if (!query || query.trim() === "") {
+        return tasks;
+    }
+
+    return tasks.filter(({ content }) =>
+        content.toUpperCase().includes(query.trim().toUpperCase()));
+}
 
 export default tasksSlice.reducer;
