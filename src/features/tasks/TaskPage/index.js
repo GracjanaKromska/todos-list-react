041fab25241e8aee8e7e00f864167fefa96daded
Gracjana Kromska <gracjana0915@wp.pl>
2023-09-04 16:09:57 +0200

    Task Page added

---
+++ src/features/tasks/TaskPage/index.js
@@ -0,0 +1,26 @@
+import Section from "../../../common/Section";
+import Header from "../../../common/Header";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getTaskById } from "../tasksSlice";
+
+function TaskPage() {
+    const { id } = useParams();
+    const task = useSelector(state => getTaskById(state, id));
+
+    return (
+        <>
+            <Header title="Szczegóły zadania" />
+            <Section
+                title={task ? task.content : "Nie znaleziono zadania."}
+                body={!!task && (
+                    <>
+                        <strong>Ukończono:</strong>: {task.done ? "Tak" : "Nie"}
+                    </>
+                )}
+            />
+        </>
+    );
+};
+
+export default TaskPage;
