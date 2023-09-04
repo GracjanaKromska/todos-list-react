76a5c4626c7afa47817186797f5770676d8edadd
Gracjana Kromska <gracjana0915@wp.pl>
2023-09-04 16:05:13 +0200

    moved to TasksPage folder

---
+++ src/features/tasks/TasksPage/Buttons/index.js
@@ -0,0 +1,31 @@
+import { useSelector, useDispatch } from "react-redux";
+import { Button, StyledButtons } from "./styled";
+import { selectEmptyTasks, selectEveryTaskDone, selectHideDone, setAllDone, toggleHideDone } from "../../tasksSlice";
+
+const Buttons = () => {
+  const emptyTasks = useSelector(selectEmptyTasks);
+  const everyTaskDone = useSelector(selectEveryTaskDone);
+  const hideDone = useSelector(selectHideDone);
+
+  const dispatch = useDispatch();
+
+  return (
+    <StyledButtons>
+      {!emptyTasks && (
+        <>
+          <Button onClick={() => dispatch(toggleHideDone())}>
+            {hideDone ? "Pokaż" : "Ukryj"} ukończone
+          </Button>
+          <Button
+            onClick={() => dispatch(setAllDone())}
+            disabled={everyTaskDone}
+          >
+            Ukończ wszystkie
+          </Button>
+        </>
+      )}
+    </StyledButtons>
+  )
+};
+
+export default Buttons;
---
+++ src/features/tasks/TasksPage/Form/index.js
@@ -0,0 +1,42 @@
+import { useState, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { Button, StyledForm } from "./styled";
+import { addTask } from "../../tasksSlice";
+import { nanoid } from "@reduxjs/toolkit";
+import Input from "../../Input";
+
+const Form = () => {
+  const [newTaskContent, setNewTaskContent] = useState("");
+  const inputRef = useRef(null);
+
+  const dispatch = useDispatch()
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    if (newTaskContent.trim() !== "")
+
+    dispatch(addTask({
+      content: newTaskContent.trim(),
+      done: false,
+      id: nanoid(),
+    }));
+
+    setNewTaskContent("");
+    inputRef.current.focus();
+  };
+
+  return (
+    <StyledForm onSubmit={onFormSubmit}>
+      <Input
+        ref={inputRef}
+        value={newTaskContent}
+        placeholder="Co jest do zrobienia?"
+        onChange={({ target }) => setNewTaskContent(target.value)}
+      />
+      <Button>Dodaj zadanie</Button>
+    </StyledForm>
+  );
+};
+
+export default Form;
